888b6ca169549cfb989584e852b38c953ab7f7e8
Dmitry Ponyatov <dponyatov@gmail.com>
2021-04-21 14:08:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- config/config.exs
+++ config/config.exs
@@ -7,13 +7,22 @@
 # General application configuration
 use Mix.Config
 
-config :w,
-  ecto_repos: [W.Repo]
+# , W.Psql]
+config :w, ecto_repos: [W.Repo]
+
+config :w, W.Psql,
+  username: "zay",
+  password: "testpass",
+  database: "zay",
+  hostname: "localhost",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
 
 # Configures the endpoint
 config :w, WWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "gpUkga2K86D6hqxZXfbMEU8mdQ+l052LBi6AIFoJZ8p7fYtKPgQpk8yQAZRIcCrz",
+  http: [port: 54321],
+  url: [host: "localhost", port: 54321],
+  secret_key_base: "gpUkga2K86D6hqxZXyQAZRIcCrz",
   render_errors: [view: WWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: W.PubSub,
   live_view: [signing_salt: "c38gev6t"]
@@ -21,7 +30,8 @@ config :w, WWeb.Endpoint,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:mfa, :request_id],
+  level: :debug
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
